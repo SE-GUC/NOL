a025028dae18cde21d43da55b5e794c3c401b02e
haniahany <hania.elbaathy@student.guc.edu.eg>
2019-03-02 23:13:43 +0200

    Added Contact Us entity

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const contactUsController= require('./controllers/contactUsController')
+app.use('/api/contactus', contactUsController)
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Welcome</h1>`)
+})
+
+const port = process.env.PORT | 3000
+app.listen(port, () => console.log(`Server up and running on port ${port}`))
