5b4d773c105f524f8ca259b5f6042bfa712ab2f3
sarahelfouly <sarahelfouly123@gmail.com>
2019-03-02 21:33:21 +0200

    Added AWG_Admin and Announcement entities

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.use(express.json())
+
+const announecement = require('./controllers/announcement')
+const AWG_Admin = require('./controllers/AWG_Admin')
+
+
+
+app.use('/api/AWG_Admins', AWG_Admin)
+app.use('/api/Announcements', announecement)
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Welcome</h1>`)
+})
+
+
+
+
+const port = process.env.PORT | 3000
+app.listen(port, () => console.log(`Server up and running on port ${port}`))
