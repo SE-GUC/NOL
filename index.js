7d48fe1a8c2a495027277b92313b5ea365c78f2e
Noha1998 <nohaamr36@yahoo.com>
2019-03-03 14:28:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,8 @@ const app = express()
 app.use(express.json())
 
 
-const aboutus= require('./controllers/aboutus')
-app.use('/api/Aboutus',aboutus )
-
-const munadmin= require('./controllers/MUNadmin')
-app.use('/api/MUNadmins' , munadmin )
-
-const committiee = require('./controllers/Committiee')
-app.use('/api/committiees', committiee)
-
-const AWG_AboutUs = require('./controllers/AWG_AboutUs')
-app.use('/api/awg_about_us', AWG_AboutUs)
-
-const userController = require('./controllers/userController')
-app.use('/api/users', userController)
-
 const Merchandise = require('./controllers/Merchandise')
 app.use('/api/merhcandise', Merchandise)
-
-const Events = require('./controller/events')
-app.use('/api/events', Events)
 
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome</h1>`)
