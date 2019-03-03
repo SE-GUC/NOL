f9e5130401a777eeef6e76bb0102e980d44c1207
Noha1998 <nohaamr36@yahoo.com>
2019-03-03 14:28:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,6 @@ app.use('/api/Aboutus',aboutus )
 const munadmin= require('./controllers/MUNadmin')
 app.use('/api/MUNadmins' , munadmin )
 
-const committiee = require('./controllers/Committiee')
-app.use('/api/committiees', committiee)
-
-const AWG_AboutUs = require('./controllers/AWG_AboutUs')
-app.use('/api/awg_about_us', AWG_AboutUs)
-
-const userController = require('./controllers/userController')
-app.use('/api/users', userController)
 
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome</h1>`)
