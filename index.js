7a02788ab234f650ad30fda863f460d063e44766
Noha1998 <nohaamr36@yahoo.com>
2019-03-23 02:10:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-
-const express = require('express');
-var app = express();
-
-const path = require('path');
-
-const { mongoose } = require('./db.js');
 const contactusfunctions = require('./controllers/contactusfunctions')
 
-app.use(express.json())
 
 app.use('/contactus', contactusfunctions)
 
@@ -15,7 +7,6 @@ app.get('/', (req, res) => {
     res.send(`<h1>Welcome</h1>`)
 })
 
-const port = process.env.PORT | 3000
-app.listen(port, () => console.log(`Server up and running on port ${port}`))
+
 
 
