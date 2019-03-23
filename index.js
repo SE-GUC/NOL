1aa2a4c8de41af79c505f6582a46599c7b2b5efb
SeifDarwish <seifeldintarek@gmail.com>
2019-03-23 13:27:17 +0200

    new

--- index.js
+++ index.js
@@ -1,48 +1,21 @@
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+
+const { mongoose } = require('./db.js');
+const galleryfunctions = require('./controllers/galleryfunctions')
+
+app.use('/galleries', galleryfunctions)
 app.use(express.json())
-
-
-const aboutus= require('./controllers/aboutus')
-app.use('/api/Aboutus',aboutus )
-
-const munadmin= require('./controllers/MUNadmin')
-app.use('/api/MUNadmins' , munadmin )
-
-const committiee = require('./controllers/Committiee')
-app.use('/api/committiees', committiee)
-
-const AWG_AboutUs = require('./controllers/AWG_AboutUs')
-app.use('/api/awg_about_us', AWG_AboutUs)
-
-const userController = require('./controllers/userController')
-app.use('/api/users', userController)
-
-const Merchandise = require('./controllers/Merchandise')
-app.use('/api/merhcandise', Merchandise)
-
-const Events = require('./controller/events')
-app.use('/api/events', Events)
-
-const subdomain = require('./controllers/subdomain')
-const FAQs = require('./controllers/FAQs')
-app.use('/api/subdomains', subdomain)
-app.use('/api/FAQs', FAQs)
-
-const announecement = require('./controllers/announcement')
-const AWG_Admin = require('./controllers/AWG_Admin')
-app.use('/api/AWG_Admins', AWG_Admin)
-app.use('/api/Announcements', announecement)
-
-const contactUsController= require('./controllers/contactUsController')
-app.use('/api/contactus', contactUsController)
 
 
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome</h1>`)
 })
 
+
 const port = process.env.PORT | 3000
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
+
 
