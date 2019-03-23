0825a85c0b8aca6611150fb2738236252ed8cc9b
sarahelfouly <sarahelfouly123@gmail.com>
2019-03-23 16:48:02 +0200

    ss

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+const config = require('config');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const AWGsignup = require('./controllers/AWGadmin');
+const AWGadmin = require('./controllers/AWGadminControl');
+const announcement = require('./controllers/announcementFunctions');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { mongoose } = require('./db.js');
 
-const express = require('express')
-const app = express()
-app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+ 
+if (!config.get('PrivateKey')) {
+    console.error('FATAL ERROR: PrivateKey is not defined.');
+    process.exit(1);
+}
+ 
+app.use(express.json());
+app.use('/AWG/signup/admin', AWGsignup);
+app.use('/AWG/signin/admin', AWGadmin );
+app.use('/announcements', announcement)
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
-
-const announecement = require('./controllers/announcement')
-const AWG_Admin = require('./controllers/AWG_Admin')
-app.use('/api/AWG_Admins', AWG_Admin)
-app.use('/api/Announcements', announecement)
-
-app.get('/', (req, res) => {
-    res.send(`<h1>Welcome</h1>`)
-})
-
-
-const port = process.env.PORT | 3000
-app.listen(port, () => console.log(`Server up and running on port ${port}`))
 
