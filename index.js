bccd7639e2da90effc508477e2e8acd1c487c04a
noha1998 <nohaamr36@yahoo.com>
2019-03-23 14:46:51 +0200

    r

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const express = require('express');
-const mongoose = require('./db.js');
-const bodyParser = require('body-parser');
-const app = express()
-
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-const DocumentController = require('./controllers/DocumentController')
+const config = require('config');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const users = require('./controllers/users');
+const admin = require('./controllers/adminControl');
+const user = require('./controllers/userControl');
 app.use('/api/documents', DocumentController)
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { mongoose } = require('./db.js');
+
 
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome</h1>`)
 })
 
-const port = process.env.PORT | 3000
-app.listen(port, () => console.log(`Server up and running on port ${port}`))
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+ 
+if (!config.get('PrivateKey')) {
+    console.error('FATAL ERROR: PrivateKey is not defined.');
+    process.exit(1);
+}
+ 
+app.use(express.json());
+app.use('/AWG/signup', users);
+app.use('/AWG/signin/admin', admin);
+app.use('/AWG/signin/user', user);
+const DocumentController = require('./controllers/DocumentController')
+ 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
