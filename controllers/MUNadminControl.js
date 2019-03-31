b06fe8c00520bafee5e917bb828a4e0565c9fa7a
reemmohey <reemmoheyy@gmail.com>
2019-03-31 19:27:44 +0200

    sprint3

--- controllers/MUNadminControl.js
+++ controllers/MUNadminControl.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const { users } = require('../models/user');
-const enc = require('../middleware/auth');
+//const enc = require('../middleware/auth');
 var { MUNusers } = require('../models/MUNuserController');
 var { faqs } = require('../models/faqsController');
 const express = require('express');
@@ -11,10 +11,34 @@ const router = express.Router();
 const ObjectId = require('mongoose').Types.ObjectId;
 
 
-router.get('/getallfaqs', enc, (req, res) => {
+router.get('/getallfaqs', (req, res) => {
     faqs.find((err, docs) => {
         if (!err) { res.send(docs); }
         else { console.log('Error in Retriving FAQs :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
+
+router.get('/getspecificfaq/:id',  (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+    faqs.findById(req.params.id, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Retriving FAQ :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
+
+router.put('/updatefaq/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+    var faq = {
+        question: req.body.question,
+        answer: req.body.answer,
+    };
+    faqs.findByIdAndUpdate(req.params.id, { $set: faq }, { new: true }, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in FAQ Update :' + JSON.stringify(err, undefined, 2)); }
     });
 });
 
--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 
 const express = require('express');
 var app = express();
+const cors = require('cors')
 
 const path = require('path');
 
 const { mongoose } = require('./db.js');
+const subdomainfunctions = require('./controllers/subdomainfunctions');
+const faqsfunctions = require('./controllers/faqsfunctions');
 const MUNadminControl= require('./controllers/MUNadminControl');
 const MUNuserControl = require('./controllers/MUNuserControl');
-const admin = require('./controllers/adminControl');
+const admin = require('./controllers/AWGadminControl');
 
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors())
 
+app.use('/subdomain', subdomainfunctions);
+app.use('/faqs', faqsfunctions);
 app.use('/MUN/signin/MUNusers', MUNuserControl );
 app.use('/MUN/signin/MUNadmins', MUNadminControl);
 app.use('/AWG/signin/admin', admin);
