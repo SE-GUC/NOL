203e36325428fdd484dac9435cc7a28bf376445d
noha1998 <noha.fakhry@student.guc.edu.eg>
2019-04-06 19:48:01 +0200

    auth

--- models/user.js
+++ models/user.js
@@ -1,35 +1,24 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
- 
-const users = mongoose.model('users', new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 255,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 1024
-    }
-}));
- 
-function validateUser(user) {
-    const schema = {
-        name: Joi.string().max(50).required(),
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required()
-    };
-    return Joi.validate(user, schema);
-}
- 
-exports.users = users;
-exports.validate = validateUser;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
