4615605db1f8117250ad0321d973d950e04b3fff
Seif Darwish <47500138+SeifDariwsh@users.noreply.github.com>
2019-04-07 16:49:01 +0200

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,75 +1,19 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
-import { Provider } from "react-redux";
-import store from "./store";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Gallery from './components/gallery'
 
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import PrivateRoute from "./components/private-route/PrivateRoute";
-import Dashboard from "./components/dashboard/Dashboard";
-import Main from './components/main'
-import MUNSignUp from './components/munsignup'
-import MUNSignIn from './components/munsignin'
-import Home from './components/home'
-import Events from './components/events'
-import Home2 from './components/home2'
-
-import "./App.css";
-
-// Check for token to keep user logged in
-if (localStorage.jwtToken) {
-  // Set auth token header auth
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  // Decode token and get user info and exp
-  const decoded = jwt_decode(token);
-  // Set user and isAuthenticated
- store.dispatch(setCurrentUser(decoded));
-  // Check for expired token
-  const currentTime = Date.now() / 1000; // to get in milliseconds
-  if (decoded.exp < currentTime) {
-    // Logout user
-   store.dispatch(logoutUser());
-
-    // Redirect to login
-    window.location.href = "./login";
-  }
-}
 class App extends Component {
   render() {
     return (
-      <Provider store= {store}>
+      <div className="App">
         <Router>
-          <div className="App">
-            <Navbar />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <Switch>
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-            </Switch>
-          </div>
-          <div className="maggie">
-          <Route path="/" component={Main} />
-          <Route path="/munsignup" component={MUNSignUp} />
-          <Route exact path="/munsignin" component={MUNSignIn} />
-          </div>
-          <div className="sara">
-          <Route exact path="/" component={Home} />
-          <Route exact path="/events" component={Events} />
-          </div>
-          <div className="hania">
-          <Route exact path="/" component={Home2} />
-          <Route exact path="/contactus" component={ContactUs} />
-          </div>
+          <Route path="/gallery" component={Gallery}/>
         </Router>
-      </Provider>
+      </div>
     );
   }
 }
+
 export default App;
--- server.js
+++ server.js
@@ -1,45 +1,27 @@
-const express = require("express");
-const app = express();
-const config = require('config');
 const { mongoose } = require('./db.js');
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const config = require('config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const cors = require('cors')
-var Joi = require('joi');
+const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
-const users = require("./routes/api/publicRoute");
-const MUNadminControl= require('./routes/api/MUNadminControl');
-const signup = require('./routes/api/MUNsignup');
-const galleryfunctions = require('./routes/api/galleryfunctions');
-const contactusfunctions= require('./routes/api/contactusfunctions');
-
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-
-if (!config.get('PrivateKey')) {
-  console.error('FATAL ERROR: PrivateKey is not defined.');
-  process.exit(1);
-}
+app.use(cors())
 
 app.use(bodyParser.json());
-// Passport middleware
-app.use(passport.initialize());
-app.use(cors())
+ 
+ if (!config.get('PrivateKey')) {
+     console.error('FATAL ERROR: PrivateKey is not defined.');
+     process.exit(1);
+ }
+
+const galleryfunctions = require('./routes/api/galleryfunctions')
+
 app.use(express.json());
+app.use('/galleries', galleryfunctions)
 
-// Passport config
-require("./config/passport")(passport);
 
-// Routes
-app.use("/api/users", users);
-app.use('/MUN/signup', signup);
-app.use('/MUN/signin/MUNadmins', MUNadminControl);
-app.use('/galleries', galleryfunctions);
-app.use('/contactus', contactusfunctions)
+const port = process.env.PORT | 3000
+app.listen(port, () => console.log(`Server up and running on port ${port}`))
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
